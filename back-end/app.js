9b59ae520c9edbcb9c0d4a74f1e1c02dac4b05ec
staggel <el17863@mail.ntua.gr>
2020-12-30 21:04:28 +0200

    Connect API to database

--- back-end/app.js
+++ back-end/app.js
@@ -2,16 +2,24 @@ const express = require('express');
 const https = require('https');
 const fs = require('fs');
 const config=require('./config.js');
-
+const mysql=require('mysql');
+const db=require('./db.js');
 
 // SSL Certifcate 
 var options = {
   key: fs.readFileSync(config.ssl_key_path),
   cert: fs.readFileSync(config.ssl_cert_path)
 };
+//Connect to database
+db.connection.connect(function(err) {
+    if (err) {
+        console.error('Error connecting: ' + err.stack);
+        return;
+    }
 
+});
 
-var app = express();
+const app = express();
 
 
 // Create HTTPS server 
--- back-end/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    ssl_key_path: "./key.pem",
-    ssl_cert_path: "./cert.pem",
-    port: 8765
-};
