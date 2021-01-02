60c54ac5bed3075bfb5144d2be758c19a2aaf189
Markos Girgis <el17870@mail.ntua.gr>
2021-01-02 14:58:52 +0200

    Create directory structure and integrate setuptools

---
+++ cli-client/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cli-tool',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        ev_group42=pkg.cli:main
+        ''',
+)
